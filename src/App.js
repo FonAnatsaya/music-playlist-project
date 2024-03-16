65817d9bf9a72e6690b57bf73e5bef4740b251e2
fon <anatsaya.lee@gmail.com>
2024-03-16 21:39:18 +0700

    Extract token from the href url and pass it as an argument of dispatch function to set token state value

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Login from "./components/Login/Login";
+import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/reducerCases";
 
 export default function App() {
+  const [{ token }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split("&")[0].split("=")[1];
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
   return (
     <div>
       <Login />
