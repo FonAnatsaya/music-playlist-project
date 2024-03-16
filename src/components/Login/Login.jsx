631a154b0128059094e6d163aecefdcbd96af7a3
fon <anatsaya.lee@gmail.com>
2024-03-16 16:04:43 +0700

    Create url to send api to third party(spotify) and save responsed token to href

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import logo from "../../assets/music_logo_free.png";
 import "./style.css";
 
+const handleButtonClicked = () => {
+  const apiUrl = "https://accounts.spotify.com/authorize";
+  const redirectUrl = "http://localhost:3000/";
+  const clientID = "37aa9bed921840bdb0e696daf4705c38";
+  const scope = [
+    "user-read-private",
+    "user-read-email",
+    "user-modify-playback-state",
+    "user-read-playback-state",
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-top-read",
+  ];
+  window.location.href = `${apiUrl}?client_id=${clientID}&redirect_uri=${redirectUrl}&scope=${scope.join(
+    "%20"
+  )}&response_type=token&show_dialog=true`;
+};
+
 export default function Login() {
   return (
     <div className="login">
       <img src={logo} alt=""></img>
-      <button>Connect App</button>
+      <button onClick={handleButtonClicked}>Connect App</button>
     </div>
   );
 }
