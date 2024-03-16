e18db926c89081fb072875c751bfda7baec34059
fon <anatsaya.lee@gmail.com>
2024-03-16 13:28:55 +0700

    Test Login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
+import Login from "./components/Login/Login";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
