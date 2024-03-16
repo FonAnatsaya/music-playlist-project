fcfd48e384424058a702d9056190b1195cbe466c
fon <anatsaya.lee@gmail.com>
2024-03-17 01:05:37 +0700

    Successfully send API to GET current user's playlists from third party(spotify)

--- src/components/Playlists/Playlists.jsx
+++ src/components/Playlists/Playlists.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useStateProvider } from "../../utils/StateProvider";
 
 export default function Playlists() {
+  const [{ token, playlists }, dispatch] = useStateProvider();
+  useEffect(() => {
+    const getPlaylists = async () => {
+      const response = await axios.get(
+        "https://api.spotify.com/v1/me/playlists",
+        {
+          headers: {
+            Authorization: "Bearer " + token,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const { items } = response.data;
+      const playlists = items.map(({ name, id }) => {
+        return { name, id };
+      });
+      console.log(playlists);
+    };
+    getPlaylists();
+  }, [token, dispatch]);
   return <div>Playlists</div>;
 }
