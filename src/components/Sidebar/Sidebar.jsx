982cc575a5e2dc5415c34caa05fcec9b2a7b1b76
fon <anatsaya.lee@gmail.com>
2024-03-17 00:12:07 +0700

    Set Sidebar css styled

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,33 @@
 import React from "react";
 import "./style.css";
+import logo from "../../assets/music_logo_free.png";
+import { IoLibrary } from "react-icons/io5";
+import { MdHomeFilled, MdSearch } from "react-icons/md";
+import Playlists from "../Playlists/Playlists";
 
 export default function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="top__links">
+        <div className="logo">
+          <img src={logo} alt="music_logo"></img>
+        </div>
+        <ul>
+          <li>
+            <MdHomeFilled />
+            <span>Home</span>
+          </li>
+          <li>
+            <MdSearch />
+            <span>Search</span>
+          </li>
+          <li>
+            <IoLibrary />
+            <span>Your Library</span>
+          </li>
+        </ul>
+      </div>
+      <Playlists />
+    </div>
+  );
 }
