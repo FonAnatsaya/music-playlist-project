5f74d52c0edb598e19938ed3e606c1248fd20d63
fon <anatsaya.lee@gmail.com>
2024-03-17 22:15:48 +0700

    Finish Navbar css style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./style.css";
+import { FaSearch } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { useStateProvider } from "../../utils/StateProvider";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const [{ userProfile }] = useStateProvider();
+  return (
+    <div className="navbar">
+      <div className="search__bar">
+        <FaSearch />
+        <input
+          className="search__bar__input"
+          type="text"
+          placeholder="Artists, songs, or podcasts "
+        ></input>
+      </div>
+      <div className="avatar">
+        <a className="avatar__a" href="#">
+          <CgProfile />
+          <span>{userProfile?.user_Name}</span>
+        </a>
+      </div>
+    </div>
+  );
 }
