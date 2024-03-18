ab41e34f8b66b8a7533bc0c17ac8a312436ef9f9
fon <anatsaya.lee@gmail.com>
2024-03-19 00:05:47 +0700

    Add Volume component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import CurrentTrack from "../CurrentTrack/CurrentTrack";
 import PlayerControls from "../PlayerControls/PlayerControls";
+import Volume from "../Volume/Volume";
 
 export default function Footer() {
   return (
     <div className="footer">
       <CurrentTrack />
       <PlayerControls />
+      <Volume />
     </div>
   );
 }
