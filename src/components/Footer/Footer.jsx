839391e0524f619a849bd4e48f44fe10f7c330c6
fon <anatsaya.lee@gmail.com>
2024-03-18 21:58:15 +0700

    add PlayerControl component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
 import CurrentTrack from "../CurrentTrack/CurrentTrack";
+import PlayerControls from "../PlayerControls/PlayerControls";
 
 export default function Footer() {
   return (
     <div className="footer">
       <CurrentTrack />
+      <PlayerControls />
     </div>
   );
 }
